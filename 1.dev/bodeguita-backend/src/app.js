38d166cd05ed1a1d8c3e397e3e2b1b036fd591d4
ZetaGH <jose.veliz.francia@gmail.com>
2019-06-26 16:04:23 -0500

    change port for testing -> 8000 - 9090

--- 1.dev/bodeguita-backend/src/app.js
+++ 1.dev/bodeguita-backend/src/app.js
@@ -1,15 +1,34 @@
+// import express from 'express'
+// import appConfig from './config/app-config'
+// import routes from './routes/routes'
+
+// const app = express();
+// appConfig(app, true);
+// routes(app);
+// appConfig(app)
+// if(process.env.NODE_ENV !== 'test') {
+//     let port = 8000
+//     app.listen(port)
+//     console.log("Listening to port " + port)
+// }
+
+// export default app;
 import express from 'express'
 import appConfig from './config/app-config'
 import routes from './routes/routes'
 
-const app = express();
-appConfig(app, true);
-routes(app);
-appConfig(app)
-if(process.env.NODE_ENV !== 'test') {
-    let port = 8000
-    app.listen(port)
-    console.log("Listening to port " + port)
+const appFunc = function start(port) {
+    const app = express();
+    appConfig(app, true);
+    routes(app);
+    appConfig(app)
+    if(process.env.NODE_ENV !== 'test') {
+        app.listen(port)
+        console.log("Listening to port " + port)
+    }
+    return app;
 }
 
-export default app;
+appFunc(8000)
+
+export default appFunc;
